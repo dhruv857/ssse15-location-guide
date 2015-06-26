eb5137a643c10eea7cae68d84bad36ba1e18a335
dhruv857 <dg83196n@pace.edu>
2015-06-26 15:36:26 -0400

    trial commit

--- mapss.js
+++ mapss.js
@@ -4,7 +4,7 @@ function myNavFun(name,lat,lon){
     if( (navigator.platform.indexOf("iPhone") != -1)
         || (navigator.platform.indexOf("iPod") != -1)
         || (navigator.platform.indexOf("iPad") != -1))
-         window.open("comgooglemaps://?q=PacePlaza&center=comgooglemaps://?q="+name+"&center="+lat + ","+ lon);
+         window.open("comgooglemaps://?q="+name+"&center="+lat + ","+ lon);
     else
          window.open("http://maps.google.com/maps?daddr="+lat+","+lon+"&ll=");
 }
